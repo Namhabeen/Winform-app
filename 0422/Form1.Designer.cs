a66fde2836ba17715c0d972ed3b1da5e252330c2
South_under_been <s2018s27@e-mirim.hs.kr>
2020-04-22 10:22:40 +0900

    초기 커밋.

---
+++ 0422/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace _0422
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.누르지마시오 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // 누르지마시오
+            // 
+            this.누르지마시오.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.누르지마시오.Font = new System.Drawing.Font("궁서체", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.누르지마시오.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.누르지마시오.Location = new System.Drawing.Point(12, 242);
+            this.누르지마시오.Name = "누르지마시오";
+            this.누르지마시오.Size = new System.Drawing.Size(304, 119);
+            this.누르지마시오.TabIndex = 0;
+            this.누르지마시오.Text = "누르지마시오";
+            this.누르지마시오.UseVisualStyleBackColor = false;
+            this.누르지마시오.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 200);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 373);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.누르지마시오);
+            this.Name = "Form1";
+            this.Text = "헬로우 월드";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button 누르지마시오;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
